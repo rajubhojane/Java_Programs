be40919469c004b15e6fce091e0924aee9a11b6f
Raju Bhojane <Rajubhojane2004@gmail.com>
2024-08-28 00:45:02 +0530

    Add feature to improve array sum logic

---
+++ arraysum.java
@@ -0,0 +1,15 @@
+public class arraysum {
+    public static void main(String args[])
+    {
+        int[] Numbers={1,2,3,4,5,6,7,2};
+        int sum=0;
+        for(int i=0;i<Numbers.length;i++)
+        {
+            sum=sum+Numbers[i];
+            
+            
+        }
+        System.out.println(sum);
+    }
+    
+}
