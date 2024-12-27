f62ad5782f2f8f384fe3dab2b89778da30736f88
rajubhojane <RajuBhojane2004@gmail.com>
2024-12-27 18:02:31 +0530

    Command line argument program with multiple arguments

---
+++ Command_Line_args_program.java
@@ -0,0 +1,10 @@
+public class Command_Line_args_program {
+    public  static void main(String args[])
+    {
+        int n=args.length;
+        for(int i=0;i<n;i++)
+        {
+             System.out.println(args[i]);
+        }
+    }
+}
